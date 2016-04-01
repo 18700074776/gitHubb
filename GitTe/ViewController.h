fbac9d0fed90023fa4302faa6bded717744e510b
18700074776 <905250687@qq.com>
2016-04-01 14:38:54 +0800

    Initial Commit

---
+++ GitTe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTe
+//
+//  Created by  on 16/4/1.
+//  Copyright © 2016年 庞梦阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTe
+//
+//  Created by  on 16/4/1.
+//  Copyright © 2016年 庞梦阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
